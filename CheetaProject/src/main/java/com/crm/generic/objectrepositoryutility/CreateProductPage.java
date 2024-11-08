22a5caa47498db738ffafcc8597d70de76cb8543
kmyas <kmyas@YASHAWANTH>
2024-11-08 16:19:54 +0530

    Added Product Page in ObjectREpo

---
+++ CheetaProject/src/main/java/com/crm/generic/objectrepositoryutility/CreateProductPage.java
@@ -0,0 +1,24 @@
+package com.crm.generic.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateProductPage {
+WebDriver driver;
+	public CreateProductPage() {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+		PageFactory.initElements( driver,this);
+		
+	}
+	@FindBy(name="ProductsLinks")
+	private WebElement productlink;
+	public WebElement getproductlink()
+	{
+		return productlink;
+	}
+
+
+}
