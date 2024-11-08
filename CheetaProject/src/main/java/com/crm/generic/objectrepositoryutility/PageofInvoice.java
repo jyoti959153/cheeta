741f6ed4b7d6c349e1562e39797b8f8d6d5fe8d4
kmyas <kmyas@YASHAWANTH>
2024-11-08 18:12:50 +0530

    paage of inc

---
+++ CheetaProject/src/main/java/com/crm/generic/objectrepositoryutility/PageofInvoice.java
@@ -0,0 +1,10 @@
+package com.crm.generic.objectrepositoryutility;
+
+public class PageofInvoice {
+
+	public PageofInvoice() {
+		// TODO Auto-generated constructor stub
+		System.out.println("page of invoice");
+	}
+
+}
