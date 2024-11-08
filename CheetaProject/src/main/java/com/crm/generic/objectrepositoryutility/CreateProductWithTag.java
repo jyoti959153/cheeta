2d9f179e4a90fe71b1094c29675ac9e94bb84a57
admin <admin@PavithraDilip>
2024-11-08 17:54:37 +0530

    push tag page

--- CheetaProject/src/main/java/com/crm/generic/objectrepositoryutility/CreateProductWithTag.java
+++ CheetaProject/src/main/java/com/crm/generic/objectrepositoryutility/CreateProductWithTag.java
@@ -1,5 +1,9 @@
 package com.crm.generic.objectrepositoryutility;
 
 public class CreateProductWithTag {
+public void method1() {
+	System.out.println("Hello iam ready to add");
+	// TODO Auto-generated method stub
 
+}
 }
